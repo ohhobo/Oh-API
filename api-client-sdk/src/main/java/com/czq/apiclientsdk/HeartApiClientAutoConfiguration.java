bc74b817f3583eafda7601055c66dce94e03a36a
ohhobo <1301868714@qq.com>
2025-01-23 19:19:16 +0800

    update

--- api-client-sdk/src/main/java/com/czq/apiclientsdk/HeartApiClientAutoConfiguration.java
+++ api-client-sdk/src/main/java/com/czq/apiclientsdk/HeartApiClientAutoConfiguration.java
@@ -1,6 +1,8 @@
 package com.czq.apiclientsdk;
 
+import com.czq.apiclientsdk.client.DayApiClient;
 import com.czq.apiclientsdk.client.NameApiClient;
+import com.czq.apiclientsdk.client.RandomApiClient;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -20,5 +22,15 @@ public class HeartApiClientAutoConfiguration {
     @Bean
     public NameApiClient czqClient(){
         return new NameApiClient(accessKey,secretKey);
+    }
+
+    @Bean
+    public DayApiClient dayClient(){
+        return new DayApiClient(accessKey,secretKey);
+    }
+
+    @Bean
+    public RandomApiClient randomClient(){
+        return new RandomApiClient(accessKey,secretKey);
     }
 }
